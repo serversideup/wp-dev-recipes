b9d5eb54c7a446c6bcf27f8fade0c54509dc1f03
Dan Pastori <dan@521dimensions.com>
2020-09-26 10:31:47 -0700

    Added NuxtJS Sitemap. Fixes #6

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,15 @@
 import theme from '@nuxt/content-theme-docs'
 
+const createSitemapRoutes = async () => {
+  let routes = [];
+  
+  routes.push('/how-to-use-these-recipes');
+  routes.push('/general/packaging-your-plugin-or-theme-with-laravel-mix');
+  routes.push('/themes/install-tailwind-css-in-your-wordpress-theme');
+  
+  return routes;
+}
+
 export default theme({
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN
@@ -8,9 +18,18 @@ export default theme({
   i18n: {
     defaultLocale: 'en'
   },
+  modules: [
+    '@nuxtjs/sitemap'
+  ],
   buildModules: [
     ['@nuxtjs/google-analytics', {
       id: 'UA-20130511-3'
     }]
-  ]
+  ],
+  sitemap: {
+    hostname: 'https://wp-dev-recipes.serversideup.net',
+    gzip: true,
+    routes: createSitemapRoutes,
+    lastmod: '2020-09-28'
+  },
 })
